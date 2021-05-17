7181fbf376d79ba8cae1a26b39439ed375089896
Huy Duong <minhhuy.uit@gmail.com>
2021-05-17 07:12:17 -0500

    Init awesome-swiftui-example

---
+++ awesome-swiftui-example/awesome-swiftui-example/awesome_swiftui_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  awesome_swiftui_exampleApp.swift
+//  awesome-swiftui-example
+//
+//  Created by Huy D. on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct awesome_swiftui_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
